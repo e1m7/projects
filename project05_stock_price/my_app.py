8edadb897ace25379d76a75c7bc0b79c92784359
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-14 23:27:22 +0300

    Add files via upload

--- project05_stock_price/my_app.py
+++ project05_stock_price/my_app.py
@@ -13,13 +13,6 @@ st.markdown("""
 This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)
 * **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
 * **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies)
-
-S&P 500 stock market index is maintained by S&P Dow Jones Indices. It comprises 503 common stocks which are issued by 500 
-large-cap companies traded on American stock exchanges (including the 30 companies that compose the Dow Jones Industrial 
-Average). The index includes about 80 percent of the American equity market by capitalization. It is weighted by free-float 
-market capitalization, so more valuable companies account for relatively more weight in the index. The index constituents 
-and the constituent weights are updated regularly using rules published by S&P Dow Jones Indices. Although called the S&P 
-500, the index contains 503 stocks because it includes two share classes of stock from 3 of its component companies.
 """)
 
 st.sidebar.header('User Input Features')
