4e243bf9dc056ac90e04275e1ded717fcee28549
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-15 20:20:49 +0300

    Add files via upload

--- project07_iris-data/my_app.py
+++ project07_iris-data/my_app.py
@@ -12,8 +12,9 @@ This app predicts the **Iris flower** type
 
 st.sidebar.header('User Input Parameters')
 
+# Собираем данные от пользователя (pandas дата-сет)
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)      # min/max/current
     sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
     petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
     petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
@@ -26,18 +27,22 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
+# Дата-сет пользовательских данных
 df = user_input_features()
 
 st.subheader('User Input parameters')
 st.write(df)
 
+# Загружаем дата-сет про ирисы
 iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
 
+# Создаем и тренируем модель
 clf = RandomForestClassifier()
 clf.fit(X, Y)
 
+# Делаем предсказание для пользоательского набора
 prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
@@ -48,7 +53,7 @@ a1.write(iris.target_names)
 
 a2.subheader('Prediction')
 a2.write(iris.target_names[prediction])
-st.write(prediction)
+st.write(prediction[0])
 
 a3.subheader('Probability')
 a3.write(prediction_proba)
