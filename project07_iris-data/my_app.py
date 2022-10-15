0b3b798f27d9764576cbb33c1b100658b351622d
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-15 19:59:39 +0300

    Add files via upload

--- project07_iris-data/my_app.py
+++ project07_iris-data/my_app.py
@@ -43,11 +43,12 @@ prediction_proba = clf.predict_proba(df)
 
 a1, a2, a3 = st.columns(3)
 
-a1.subheader('Class labels')
+a1.subheader('🌼 Class labels')
 a1.write(iris.target_names)
 
-a2.subheader('Prediction')
+a2.subheader('🌼 Prediction')
 a2.write(iris.target_names[prediction])
+st.write(prediction)
 
-a3.subheader('Probability')
+a3.subheader('🌼 Probability')
 a3.write(prediction_proba)
