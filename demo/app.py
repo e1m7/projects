de53f1ed456d80ca0fd201a948143860053a30c9
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-16 22:55:01 +0300

    Add files via upload

--- demo/app.py
+++ demo/app.py
@@ -7,7 +7,5 @@ st.write("Hello World!")
 # пример работы командной кнопки
 if st.button('Say hello'):
     st.write('Why hello there')
-    st.button('Why hello there')
 else:
     st.write('Goodbye')
-    st.button('Goodbye')
