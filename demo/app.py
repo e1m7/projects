a95c3020b534c2df36dacee7e064dbc93a7abc21
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-16 23:00:11 +0300

    Add files via upload

--- demo/app.py
+++ demo/app.py
@@ -4,11 +4,11 @@ import streamlit as st
 # пример вывода текст
 st.write("Hello World!")
 
-def hello():
-    st.write('All you need is love')
+def hello(a):
+    st.write(f'All you need is love {a}')
 
 # пример работы командной кнопки
-if st.button('Say hello', help='Help self', on_click=hello):
+if st.button('Say hello', help='Help self', on_click=hello, args=(1,2,3)):
     st.write('Why hello there')
 else:
     st.write('Goodbye')
