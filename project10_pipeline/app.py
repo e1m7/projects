9933c5cf5d2be912e449383905d90171b5857db0
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-17 22:48:54 +0300

    Add files via upload

--- project10_pipeline/app.py
+++ project10_pipeline/app.py
@@ -12,3 +12,29 @@ st.write("## Create RDD from a Python list")
 
 l = list(range(10))
 st.write(l)
+
+rdd = sc.parallelize(l)
+rdd.cache()
+st.write(rdd)
+
+st.write("## Get results through actions")
+st.write(rdd.collect())
+st.write(rdd.take(3))
+st.write(rdd.count())
+
+st.write("## Transform RDDs")
+st.write(rdd.filter(lambda x: x%2==0).collect())  # talk about lazy evaluation here: filter still non evaluated rdd
+st.write(rdd.map(lambda x: x*2).collect()) 
+st.write(rdd.map(lambda x: x*2).reduce(lambda x, y: x + y))  # reduce runs all previous rdds
+# Compare the two following
+st.write(rdd.map(lambda x: list(range(x))).collect()) 
+st.write(rdd.flatMap(lambda x: list(range(x))).collect()) 
+
+st.write("## Wordcount")
+file_rdd = sc.textFile("lorem.txt")
+st.write(file_rdd.collect())  # so what's inside ?
+st.write(file_rdd.flatMap(lambda sentence: sentence.split()).map(lambda word: (word, 1)).reduceByKey(lambda x,y: x+y).collect())
+st.write()
+
+
+# spark.stop()
