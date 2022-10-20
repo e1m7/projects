ee99a7a1d31e19523bdd74af87cca19bd198e94d
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-20 20:54:39 +0300

    Add files via upload

--- model1/my_app.py
+++ model1/my_app.py
@@ -27,12 +27,19 @@ st.write("Nous déduisons pour 10 caractéristiques graphiques qui montrent comm
 for label in cols[:-1]:
     a = df[df["class"]==1][label]
     b = df[df["class"]==0][label]
-    st.write(a)
-    st.write(b)
+    x1 = np.random.uniform(0, 100, (1000))
+    x2 = np.random.uniform(0, 100, (2000))
+    x3 = np.random.uniform(0, 100, (3000))   
+    hist_data = [x1, x2, x3]
+    group_labels = ['Group1','Group2','Group3']
+    figejemplo = ff.create_distplot(hist_data, group_labels, 
+                             bin_size=[10, 10, 10, 10], show_hist=False, show_rug=False)
+    st.plotly_chart(figejemplo, config=config, use_container_width=True)
+
     # arr = np.random.normal(1, 1, size=100)
     # fig, ax = plt.subplots()
-    # ax.hist(arr, bins=20)
-    # st.pyplot(fig)
+    # ax.hist(a, bins=20)
+    # st.pyplot(b)
 
 #   plt.title(label)
 #   plt.ylabel("Probability")
