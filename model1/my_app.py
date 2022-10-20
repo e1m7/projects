687bcef13bfc9006ad6658d759d75437e9cbfbb8
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-20 20:44:50 +0300

    Add files via upload

--- model1/my_app.py
+++ model1/my_app.py
@@ -22,3 +22,13 @@ df["class"] = (df["class"] == "g").astype(int)
 
 st.write("Dans la Dernière colonne, remplacez les valeurs 'g| / 'h' par 0/1 afin que les modèles puissent les gérer facilement")
 st.dataframe(df.head(3))
+
+st.write("Nous déduisons pour 10 caractéristiques graphiques qui montrent comment la distribution de la Dernière colonne est liée (0,1)")
+for label in cols[:-1]:
+  plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
+  plt.hist(df[df["class"]==0][label], color='red', label='hadron', alpha=0.7, density=True)
+  plt.title(label)
+  plt.ylabel("Probability")
+  plt.xlabel(label)
+  plt.legend()
+  plt.show()
