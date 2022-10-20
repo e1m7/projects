ecb3dc56c482840119a83218bf6d6e3dbc9317d4
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-20 21:12:29 +0300

    Add files via upload

--- model1/my_app.py
+++ model1/my_app.py
@@ -51,7 +51,7 @@ valid, X_valid, y_valid = scale_dataset(valid, oversample=False)
 test, X_test, y_test = scale_dataset(test, oversample=False)
 
 st.write("Après avoir aligné les données dans le jeu d'enregistrements de test avec 0 et 1, il est devenu le même nombre")
-st.write("Maintenant dans le jeu de test d'enregistrements **gamma**", 7399))
+st.write("Maintenant dans le jeu de test d'enregistrements **gamma**", 7399)
 st.write("Maintenant, un jeu de test d'enregistrements **gudron**", 7399))
 
 number = st.slider('Sélectionnez le nombre de points adjacents', 1, 10, 1)
