23d1e771e4e1feecb25a263104c02406a60c4a75
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-20 21:03:50 +0300

    Add files via upload

--- model1/my_app.py
+++ model1/my_app.py
@@ -31,6 +31,29 @@ st.write("""
 - test = données de test, 80 à 100% des données
 """)
 train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
-st.write(train)
-st.write(valid)
-st.write(test)
+
+def scale_dataset(dataframe, oversample=False):
+    X = dataframe[dataframe.columns[:-1]].values
+    y = dataframe[dataframe.columns[-1]].values
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    if oversample:
+        ros = RandomOverSampler()
+        X, y = ros.fit_resample(X, y)
+    data = np.hstack((X, np.reshape(y, (-1, 1))))
+    return data, X, y
+
+st.write("Maintenant dans le jeu de test d'enregistrements **gamma**")
+st.write(len(train[train["class"] == 1]))
+st.write("Maintenant, un jeu de test d'enregistrements **gudron**")
+st.write(len(train[train["class"] == 0]))
+
+train, X_train, y_train = scale_dataset(train, oversample=True)
+valid, X_valid, y_valid = scale_dataset(valid, oversample=False)
+test, X_test, y_test = scale_dataset(test, oversample=False)
+
+st.write("Après avoir aligné les données dans le jeu d'enregistrements de test avec 0 et 1, il est devenu le même nombre")
+st.write("Maintenant dans le jeu de test d'enregistrements **gamma**")
+st.write(len(train[train["class"] == 1]))
+st.write("Maintenant, un jeu de test d'enregistrements **gudron**")
+st.write(len(train[train["class"] == 0]))
