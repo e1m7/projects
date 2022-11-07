820371bdf7e286d167cde57c9c47a3979a1b478a
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-11-07 21:30:45 +0300

    Add files via upload

--- model3_positive/my_app.py
+++ model3_positive/my_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn import svm
+# from sklearn.model_selection import train_test_split
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn import svm
 
 class Sentiment:
   NEGATIVE = 'NEGATIVE'
@@ -35,20 +35,20 @@ with open(file_name) as f:
 st.title("SVM-test: Positive/Negative text")
 st.write(reviews[0].text)
 
-training, test = train_test_split(reviews, test_size=0.33, random_state=42)
+# training, test = train_test_split(reviews, test_size=0.33, random_state=42)
 
-train_x = [x.text for x in training]
-train_y = [x.sentiment for x in training]
-test_x = [x.text for x in test]
-test_y = [x.sentiment for x in test]
+# train_x = [x.text for x in training]
+# train_y = [x.sentiment for x in training]
+# test_x = [x.text for x in test]
+# test_y = [x.sentiment for x in test]
 
-vectorized = CountVectorizer()
-train_x_vectors = vectorized.fit_transform(train_x)
-test_x_vectors = vectorized.transform(test_x)
+# vectorized = CountVectorizer()
+# train_x_vectors = vectorized.fit_transform(train_x)
+# test_x_vectors = vectorized.transform(test_x)
 
-clf_svm = svm.SVC(kernel='linear')
-clf_svm.fit(train_x_vectors, train_y)
+# clf_svm = svm.SVC(kernel='linear')
+# clf_svm.fit(train_x_vectors, train_y)
 
-a = clf_svm.predict(test_x_vectors[0])
-st.write(a)
+# a = clf_svm.predict(test_x_vectors[0])
+# st.write(a)
 
